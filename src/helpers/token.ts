9ffabb03bab3e26b57dd457ab783a1617670ddd3
Banda Bahari Putra <bandabahariputra99@gmail.com>
2022-04-28 12:41:53 +0700

    Add middleware

--- src/helpers/token.ts
+++ src/helpers/token.ts
@@ -1,10 +1,20 @@
-import jwt from 'jsonwebtoken';
+import jwt, {
+  JsonWebTokenError,
+  JwtPayload,
+  NotBeforeError,
+  TokenExpiredError,
+} from 'jsonwebtoken';
 import ms from 'ms';
 
 import config from '../config/config';
 
 interface Payload {
   id: string;
+}
+
+interface VerifyAccessToken {
+  message: string;
+  data: JwtPayload | string | any;
 }
 
 const generateAccessToken = (payload: Payload): string => {
@@ -20,4 +30,38 @@ const generateAccessToken = (payload: Payload): string => {
   return accessToken;
 };
 
-export { generateAccessToken };
+const getToken = (headers: any): string => {
+  if (headers.authorization) {
+    const token = headers.authorization;
+
+    return token;
+  }
+
+  return '';
+};
+
+const verifyAccessToken = (token: string): VerifyAccessToken | undefined => {
+  try {
+    if (!token) {
+      return { message: 'Unauthorized!', data: null };
+    }
+
+    const data = jwt.verify(token, config.JWT_SECRET_ACCESS_TOKEN);
+
+    return { message: 'Token Verified', data };
+  } catch (error: any) {
+    if (error instanceof NotBeforeError) {
+      return { message: `JWT Not Before Error: ${error.message}`, data: null };
+    }
+
+    if (error instanceof JsonWebTokenError) {
+      return { message: `JWT Token Error: ${error.message}`, data: null };
+    }
+
+    if (error instanceof TokenExpiredError) {
+      return { message: `JWT Expired Error: ${error.message}`, data: null };
+    }
+  }
+};
+
+export { generateAccessToken, getToken, verifyAccessToken };
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,23 +1,45 @@
 import express from 'express';
 
 import UserController from '../controllers/user.controller';
+import Auth from '../middlewares/auth';
 import Validation from '../middlewares/validation';
 import userValidation from '../validations/user.validation';
 
 const router = express.Router();
 
+const auth = new Auth();
 const validation = new Validation();
 const userController = new UserController();
 
 router
   .route('/')
-  .get(validation.run(userValidation.getUsers), userController.getUsers)
-  .post(validation.run(userValidation.createUser), userController.createUser);
+  .get(
+    auth.isLoggedIn,
+    validation.run(userValidation.getUsers),
+    userController.getUsers,
+  )
+  .post(
+    auth.isLoggedIn,
+    validation.run(userValidation.createUser),
+    userController.createUser,
+  );
 
 router
   .route('/:id')
-  .get(validation.run(userValidation.getUser), userController.getUser)
-  .patch(validation.run(userValidation.updateUser), userController.updateUser)
-  .delete(validation.run(userValidation.getUser), userController.deleteUser);
+  .get(
+    auth.isLoggedIn,
+    validation.run(userValidation.getUser),
+    userController.getUser,
+  )
+  .patch(
+    auth.isLoggedIn,
+    validation.run(userValidation.updateUser),
+    userController.updateUser,
+  )
+  .delete(
+    auth.isLoggedIn,
+    validation.run(userValidation.getUser),
+    userController.deleteUser,
+  );
 
 export default router;
